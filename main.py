3d3cf5cd42fcf414928fda8b818829013b3f093a
Sebastian <sebastiansloma@gmail.com>
2022-12-14 17:11:28 +0100

    add lablels

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ root = Tk()
 root.geometry('600x500')
 root.title('Registration')
 
+
+label_0 = Label(root, text="Registration form", width=20, font=("bold", 20))
+label_0.place(x=90, y=53)
+
+label_1 = Label(root, text="FullName", width=20, font=("bold", 10))
+label_1.place(x=80, y=130)
+
+entry_1 = Entry(root, textvar=Fullname)
+entry_1.place(x=240, y=130)
+
 root.mainloop()
