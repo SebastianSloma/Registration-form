164f955a277d8a398e26148840a2a66c56f0cdb9
Sebastian <sebastiansloma@gmail.com>
2022-12-15 17:00:42 +0100

    add list, checkbutton and submit

--- main.py
+++ main.py
@@ -37,21 +37,37 @@ label_1.place(x=80, y=130)
 entry_1 = Entry(root, textvar=full_name)
 entry_1.place(x=240, y=130)
 
-label_2 = Label(root, text="Email",width=20,font=("bold", 10))
-label_2.place(x=68,y=180)
- 
-entry_2 = Entry(root,textvar=Email)
-entry_2.place(x=240,y=180)
- 
-label_3 = Label(root, text="Gender",width=20,font=("bold", 10))
-label_3.place(x=70,y=230)
+label_2 = Label(root, text="Email", width=20, font=("bold", 10))
+label_2.place(x=68, y=180)
 
-Radiobutton(root, text="Male",padx = 5, variable=var, value=1).place(x=235,y=230)
-Radiobutton(root, text="Female",padx = 20, variable=var, value=2).place(x=290,y=230)
+entry_2 = Entry(root, textvar=Email)
+entry_2.place(x=240, y=180)
 
-label_4 = Label(root, text="country",width=20,font=("bold", 10))
-label_4.place(x=70,y=280)
+label_3 = Label(root, text="Gender", width=20, font=("bold", 10))
+label_3.place(x=70, y=230)
 
-list1 = ['Poland','Espania','England','Germany','France','Italy'];
+Radiobutton(root, text="Male", padx=5, variable=var,
+            value=1).place(x=235, y=230)
+Radiobutton(root, text="Female", padx=20,
+            variable=var, value=2).place(x=290, y=230)
+
+label_4 = Label(root, text="Country", width=20, font=("bold", 10))
+label_4.place(x=70, y=280)
+
+list1 = ['Poland', 'Espania', 'England', 'Germany', 'France', 'Italy']
+
+droplist = OptionMenu(root, c, *list1)
+droplist.config(width=15)
+c.set('select your country')
+droplist.place(x=240, y=280)
+
+label_4 = Label(root, text="Drink", width=20, font=("bold", 10))
+label_4.place(x=85, y=330)
+var2 = IntVar()
+Checkbutton(root, text="Coffee", variable=var1).place(x=290, y=330)
+Checkbutton(root, text="Tea", variable=var2).place(x=235, y=330)
+
+Button(root, text='Submit', width=20, bg='grey',
+       fg='white', command=database).place(x=180, y=380)
 
 root.mainloop()
